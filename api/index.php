44e26492707fa2a23315388cd14be3bf95f396d3
malaz22mm <malazmalaz71@gmail.com>
2026-06-06 15:53:38 +0300

    api

--- api/index.php
+++ api/index.php
@@ -1,9 +1,22 @@
 <?php
 
+// TEMPORARY DEBUG - REMOVE AFTER FIXING
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 define('LARAVEL_START', microtime(true));
 
 $root = dirname(__DIR__);
 chdir($root);
+
+// Check files exist
+if (!file_exists($root . '/vendor/autoload.php')) {
+    die(json_encode(['error' => 'vendor/autoload.php not found', 'root' => $root]));
+}
+
+if (!file_exists($root . '/bootstrap/app.php')) {
+    die(json_encode(['error' => 'bootstrap/app.php not found', 'root' => $root]));
+}
 
 require $root . '/vendor/autoload.php';
 
